a2a758c320e347cb6f0a509504efd3af99eff536
김예빈 <dpqls161@gmail.com>
2026-08-09 03:53:45 +0900

    feat: 홈 메인 스와이프 전 화면 UI 구현

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,41 @@
-import homeBackground from "@/assets/images/custom/home-background.svg";
+import homeDivider from "@/assets/images/custom/home-devider.svg";
 import "./Home.css";
 
 const Home = () => {
   return (
     <main className="home-container">
-      <img
-        className="home-background"
-        src={homeBackground}
-        alt=""
-        aria-hidden="true"
-      />
+      <div className="home-design">
+        <section className="home-info">
+          <h1 className="home-title">
+            MAGGIE'S
+            <br />
+            GARDEN
+          </h1>
 
+          <img
+            className="home-divider"
+            src={homeDivider}
+            alt=""
+            aria-hidden="true"
+          />
+
+          <p className="home-subtitle">
+            pouring love and letters
+          </p>
+
+          <img
+            className="home-divider"
+            src={homeDivider}
+            alt=""
+            aria-hidden="true"
+          />
+
+          <div className="home-performance-info">
+            <p>2026.10.15(목) 20:00</p>
+            <p>살롱문보우</p>
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
