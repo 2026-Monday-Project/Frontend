a1e3f834293519574652973b2b0130b4b989912e
daniswings <dain@duksung.ac.kr>
2026-08-25 20:38:26 +0900

    feat: 편지함 목록 화면 하드코딩

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home/Home";
-import Login from "@/pages/Login/Login";
-import LoginCompleted from "@/pages/Login/LoginCompleted";
+import Router from "@/router/Router";
+import "./App.css";
 
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/login-completed" element={<LoginCompleted />} />
-      </Routes>
-    </BrowserRouter>
-  );
+const App = () => {
+    return (
+        <Router />
+    );
 };
 
-export default Router;
+export default App;
